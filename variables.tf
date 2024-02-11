6e81fd212f3487461e917eb0f4ebd1640ca05c9b
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-11 13:08:04 -0600

    create task definition

--- variables.tf
+++ variables.tf
@@ -21,3 +21,18 @@ variable "db_password" {
 variable "bastion_key_name" {
   default = "key_pair_name_from_aws"
 }
+
+variable "ecr_image_api" {
+  description = "ECR Image for API"
+  default     = "<APP ECR Image URL>:latest"
+}
+
+variable "ecr_image_proxy" {
+  description = "ECR Image for proxy"
+  default     = "<App ECR Image for Proxy>:latest"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django app"
+}
+
