db153ea94736c8f6447fa7a02dfd9b0b2376410d
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-11 14:25:07 -0600

    create permission for ecs to s3

--- ecs.tf
+++ ecs.tf
@@ -129,3 +129,24 @@ resource "aws_ecs_service" "api" {
   }
 
 }
+
+data "template_file" "ecs_s3_write_policy" {
+  template = file("./templates/ecs/s3-write-policy.json.tpl")
+
+  vars = {
+    bucket_arn = aws_s3_bucket.app_public_files.arn
+  }
+}
+
+resource "aws_iam_policy" "ecs_s3_access" {
+  name        = "${local.prefix}-AppS3AccessPolicy"
+  path        = "/"
+  description = "Allow access to the recipe app S3 bucket"
+
+  policy = data.template_file.ecs_s3_write_policy.rendered
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_s3_access" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = aws_iam_policy.ecs_s3_access.arn
+}
